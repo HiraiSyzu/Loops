f5cb64a75ad0f66b9f568df6e1c14d2d95c7f2f3
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-26 09:35:05 +0800

    Update 4.Counting-Even-Numbers-1-to-10.java

--- 1. For loop/4.Counting-Even-Numbers-1-to-10.java
+++ 1. For loop/4.Counting-Even-Numbers-1-to-10.java
@@ -9,6 +9,7 @@ public class LoopTest {
   public static void main(String[] args) {
   
     int sum = 0;
+    
     for (int i = 1; i <= 10; i++) {
       if (i % 2 == 0) {
         sum++;
@@ -18,6 +19,6 @@ public class LoopTest {
   }
 }
 // My Explanation:
-// In this case, we count the even numbers between 1 and 10 by checking if each number is divisible by 2 (i % 2 == 0). The loop increments the sum each time it finds an even number. 
+// In this case, we will count how many are  the even numbers between 1 and 10 by checking if each number is divisible by 2 (i % 2 == 0). The loop increments (i++) the sum each time it finds an even number. 
 // The final count will be 5 (the even numbers are 2, 4, 6, 8, and 10).
 
