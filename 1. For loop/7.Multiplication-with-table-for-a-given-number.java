29d3d13b6851a4e3587f23ec889313b1cb1049b4
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-26 09:38:41 +0800

    Update 7.Multiplication-with-table-for-a-given-number.java

--- 1. For loop/7.Multiplication-with-table-for-a-given-number.java
+++ 1. For loop/7.Multiplication-with-table-for-a-given-number.java
@@ -1,8 +1,8 @@
-//Syzu 4/25/25
+// Syzu 4/25/25
 
 // Question 7: "Multiplication Table for a Given Number"
 // Instruction:
-// Write a loop that generates the multiplication table for a given number (e.g., 7). The output should show the results of multiplying the number by 1 through 10.
+// Write a loop that generates the multiplication table for a given number (ex. 7). The output should show the results of multiplying the number by 1 through 10.
 
 public class LoopTest {
 
@@ -18,5 +18,5 @@ public class LoopTest {
 }
 // My Explanation:
 // We assign the variable int num with the value of 7. The loop starts with int i = 1, and the condition i <= 10 means the loop will run until i reaches 10. 
-// For each i, we print the value of num, concatenate "x", the loop index i, and then the result of num * i. The output will display the multiplication table for the number 7, starting from 7x1 = 7 to 7x10 = 70.
+// For each i, it will print the value of num, we will concatenate "x", the loop index i, and then the result of num * i. The output will show the multiplication table for the number 7, starting from 7x1 = 7 to 7x10 = 70.
 
