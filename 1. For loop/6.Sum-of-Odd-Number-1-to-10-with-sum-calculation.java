8f9d745753195401aae09eee930755fc65a3aee6
HiraiSyzu <hiraisyzu12@Gmail.com>
2025-04-26 09:37:14 +0800

    Update 6.Sum-of-Odd-Number-1-to-10-with-sum-calculation.java

--- 1. For loop/6.Sum-of-Odd-Number-1-to-10-with-sum-calculation.java
+++ 1. For loop/6.Sum-of-Odd-Number-1-to-10-with-sum-calculation.java
@@ -1,6 +1,6 @@
-//Syzu 4/25/25
+// Syzu 4/25/25
 
-//Question 6: "Sum of Odd Numbers from 1 to 10 (with Sum Calculation)"
+// Question 6: "Sum of Odd Numbers from 1 to 10 (with Sum Calculation)"
 // Instruction:
 // Use a for loop to add the odd numbers between 1 and 10. Instead of counting, you will calculate the sum of these odd numbers.
 
@@ -17,5 +17,5 @@ public class LoopTest {
     System.out.println("The total sum of odd numbers is: " + sum);
   }
 }
-// Your Explanation:
-// This is similar to the previous question, but instead of counting the odd numbers, we add them together. The loop will add the odd numbers (1, 3, 5, 7, 9) to the sum variable. The result will be 25.
+// My Explanation:
+// This is just the same to the previous question, but instead of counting how many are the odd numbers, we will add them together. The loop will add the odd numbers (1, 3, 5, 7, 9) to the sum variable. The result will be 25.
